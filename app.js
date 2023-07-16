6da7f1660ffc4fc0e843dbccfe1e29b1b551d932
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-07-17 00:43:41 +0530

    established mongo connection

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
+
+//Mongoose Connection
+mongoose.set('strictQuery', false); // Prepare for Mongoose 7
+
+main().catch((err) => console.log(err));
+
+async function main() {
+	await mongoose.connect(process.env.Mongo_URL);
+}
 
 var app = express();
 
