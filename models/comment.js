2829ea5d9a60c3bb5d4eb1a8b1a237584b64f175
MiMic06 <rohit.molawade27@gmail.com>
2023-07-31 16:06:09 +0530

    created virtual properties for Post and comments.

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
+const intlFormat = require('date-fns/intlFormat');
 
 const Schema = mongoose.Schema;
 
 const commentschema = new Schema({
 	name: { type: String, required: true },
-	post: { type: Schema.Types.ObjectId, required: true },
+	post: { type: Schema.Types.ObjectId, required: true, refs: 'Post'},
 	content: { type: String, required: true },
 	timestamp: { type: Date, required: true, default: Date.now() },
+},
+{
+	toObject: { virtuals: true, getters: true },
+	toJSON: { virtuals: true, getters: true },
+}
+);
+
+commentschema.virtual('date').get(function () {
+	return intlFormat(this.timestamp, { year: 'numeric', month: 'long', day: 'numeric' });
+});
+
+commentschema.virtual('time').get(function () {
+	return intlFormat(this.timestamp, { hour: 'numeric', minute: 'numeric' });
 });
 
 module.exports = mongoose.model('Comment', commentschema);
--- models/post.js
+++ models/post.js
@@ -1,15 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const intlFormat = require('date-fns/intlFormat');
 
 const Schema = mongoose.Schema;
 
 const postschema = new Schema({
-	author: { type: Schema.Types.ObjectId, required: true, refs: 'User' },
-	timestamp: { type: Date, required: true },
-	title: { type: String, required: true, maxLength: 48 },
-	content: { type: String, required: true },
-	banner_image_url: { type: String, required: true },
-	published: { type: Boolean, required: true },
-	published_time: { type: Date, required: true },
+  author: { type: Schema.Types.ObjectId, required: true, refs: "User" },
+  timestamp: { type: Date, required: true },
+  title: { type: String, required: true, maxLength: 48 },
+  content: { type: String, required: true },
+  banner_image_url: { type: String, required: true },
+  published: { type: Boolean, required: true },
+  published_time: { type: Date, required: true },
+},
+{
+	toObject: { virtuals: true, getters: true },
+	toJSON: { virtuals: true, getters: true },
+}
+);
+
+postschema.virtual("v_banner_image_url").get(function () {
+  return `http://localhost:3001${this.banner_image_url}`;
 });
 
-module.exports = mongoose.model('Post', postschema);
+postschema.virtual('date').get(function () {
+	return intlFormat(this.timestamp, { year: 'numeric', month: 'long', day: 'numeric' });
+});
+
+postschema.virtual('time').get(function () {
+	return intlFormat(this.timestamp, { hour: 'numeric', minute: 'numeric' });
+});
+
+module.exports = mongoose.model("Post", postschema);
