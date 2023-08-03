a0518fda16c73f0593f8bda181cff380d811a6c4
MiMic06 <rohit.molawade27@gmail.com>
2023-08-04 02:09:29 +0530

    added virtual property to user model

--- models/user.js
+++ models/user.js
@@ -8,6 +8,15 @@ const userschema = new Schema({
 	password: { type: String, required: true },
 	profile_picture_url: { type: String, required: true },
 	about: {type: String, required: true, maxLength: 48},
-});
+},
+{
+	toObject: { virtuals: true, getters: true },
+	toJSON: { virtuals: true, getters: true },
+}
+);
+
+userschema.virtual("v_profile_picture_url").get(function () {
+	return `http://localhost:3001${this.profile_picture_url}`;
+  });
 
 module.exports = mongoose.model('User', userschema);
